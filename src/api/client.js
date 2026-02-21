bff9b64b3442959bb61c79270c3f131f380865ba
= <=>
2026-02-21 17:05:30 -0600

    I added a function for getting a list of clients (and their associated ids) for a dropdown that the admins can view.

--- src/api/client.js
+++ src/api/client.js
@@ -9,3 +9,20 @@ const client = createClient(
 
 export default client; 
 
+// A function that will be useful for creating a dropdown for admins to pick clients from
+export async function getClientsForDropdown() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON
+  )
+
+  const { data, error } = await supabase.rpc(
+    'get_clients_for_dropdown' // name of the function that was made in SQL
+  )
+
+  if (error) {
+    throw error
+  }
+
+  return data
+}
