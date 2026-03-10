ce35ea912945052dd9ddb21e96823fb3c37fa118
Hectorium17 <landerohectoru29424@gmail.com>
2026-03-09 20:30:48 -0500

    fixing a weird error from SideBar to Sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 
-import Sidebar from "@/components/SideBar";
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
